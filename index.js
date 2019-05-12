79053031adc117e7dc2747f61031e9eaa6de4d50
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-05-13 00:46:04 +0200

    comparison operators

--- index.js
+++ index.js
@@ -27,8 +27,22 @@ async function createCourse() {
   console.log(result);
 }
 
+/** Comparison query operators:
+ * eq (equal)
+ * ne (not equal)
+ * gt (greater than)
+ * gte (greater than or equal to)
+ * lt (less than)
+ * lte (less than or equal to)
+ * in
+ * nin (not in)
+ */
+
 async function getCourses() {
-  const courses = await Course.find({ author: 'Mosh', isPublished: true })
+  const courses = await Course
+    // .find({ author: 'Mosh', isPublished: true })
+    // .find({ price: { $gte: 10, $lte: 20 } })
+    .find({ price: { $in: [10, 15, 20] } })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
